216da7def42f9eb82441eadcc35944fad98eb484
cui7dr <cui7dr@gmail.com>
2021-07-06 20:49:38 +0800

    控制层类

--- src/main/java/com/cui/spr_boot_demo/controller/Controller.java
+++ src/main/java/com/cui/spr_boot_demo/controller/Controller.java
@@ -1,44 +1,14 @@
 package com.cui.spr_boot_demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.Map;
 
 @RequestMapping("demo")//类注解，指该 Java 类所在的 rest 请求都必须以这个里面设定的值开头，该类中所有的请求路径都必须以 demo 开头
 @RestController//作为注解，告诉 SpringBoot 这个类是一个 rest 请求的类
 public class Controller {
-
-    @GetMapping("method1")//这是一个 get 请求，请求路径是 demo/method1，没有入参，返回字符串 hello springboot
-    public String method1() {
-        return "hello springBoot";
-    }
-
-    @GetMapping("method2")//get 请求，接收参数 a
-    public String method2(@RequestParam("a") String a) {
-        return "hello springBoot" + a;
-    }
-
-    @GetMapping("method3")//get 请求，接收两个 integer 的参数，并将两个数值相加后返回
-    public String method3(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
-        return "hello springBoot:" + (a + b);
-    }
-
-    @GetMapping("method3")//get 请求，接收路径参数并返回
-    public String method3(@PathVariable("c") String c) {
-        return "hello springBoot:" + c;
-    }
-
-    @GetMapping("method4/{c}")//接收路径参数并返回
-    public String method4(@PathVariable("c") String c) {
-        return "hello springBoot:" + c;
-    }
-
-    @GetMapping("method4")//post 请求，接收 map 参数，无返回值
-    public void method4(@RequestBody Map map) {
-        for (Object obj : map.keySet()) {
-            System.out.println(obj + "--->" + map.get(obj));
-        }
-    }
 
     /**
      * 1.get 请求携带参数，通过 ? 后面接参数，多个参数用 & 符号拼接；
@@ -51,5 +21,58 @@ public class Controller {
      * @RequestParam 注解在参数上，请求路径使用 ? 带参数，使用 & 符号带多个参数
      * @RequestBody 注解在参数上，post 请求可用，使用 http 的请求体携带参数
      */
+    //第一日内容：定义 get/post 方法
+//    @GetMapping("method1")//这是一个 get 请求，请求路径是 demo/method1，没有入参，返回字符串 hello springboot
+//    public String method1() {
+//        return "hello springBoot";
+//    }
+//
+//    @GetMapping("method2")//get 请求，接收参数 a
+//    public String method2(@RequestParam("a") String a) {
+//        return "hello springBoot" + a;
+//    }
+//
+//    @GetMapping("method3")//get 请求，接收两个 integer 的参数，并将两个数值相加后返回
+//    public String method3(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
+//        return "hello springBoot:" + (a + b);
+//    }
+//
+//    @GetMapping("method4")//get 请求，接收路径参数并返回
+//    public String method4(@PathVariable("c") String c) {
+//        return "hello springBoot:" + c;
+//    }
+//
+//    @GetMapping("method5/{c}")//接收路径参数并返回
+//    public String method5(@PathVariable("c") String c) {
+//        return "hello springBoot:" + c;
+//    }
+//
+//    @GetMapping("method6")//post 请求，接收 map 参数，无返回值
+//    public void method6(@RequestBody Map map) {
+//        for (Object obj : map.keySet()) {
+//            System.out.println(obj + "--->" + map.get(obj));
+//        }
+//    }
+
+    /**
+     * 使用 @Value 注解变量，并通过 ${} 标注该变量对应在 .yaml 文件中的名字
+     */
+    //第二日内容：获取 .yaml 文件中的参数
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Integer age;
+
+    @Value("${birthday}")
+    private Date birthday;
+
+    @GetMapping("method")
+    public void method() {
+        System.out.println("name=>" + name);
+        System.out.println("age=>" + age);
+        System.out.println("birthday=>" + birthday);
+    }
+
 
 }
