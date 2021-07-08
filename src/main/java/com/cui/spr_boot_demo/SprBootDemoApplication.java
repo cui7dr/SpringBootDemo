8b3c9c7f8cabcef8db1a8961685c53969e7172ea
cui7dr <cui7dr@gmail.com>
2021-07-08 21:00:24 +0800

    添加注解 @EnableConfigurationProperties 接收 yaml 里的变量

--- src/main/java/com/cui/spr_boot_demo/SprBootDemoApplication.java
+++ src/main/java/com/cui/spr_boot_demo/SprBootDemoApplication.java
@@ -2,10 +2,12 @@ package com.cui.spr_boot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableConfigurationProperties
 @SpringBootApplication
 public class SprBootDemoApplication {
 
