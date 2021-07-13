a8d33c5760bc823bdafd0506c6b82c3ae0d25472
cui7dr <cui7dr@gmail.com>
2021-07-13 21:00:55 +0800

    不再使用

--- src/main/java/com/cui/spr_boot_demo/controller/Controller.java
+++ src/main/java/com/cui/spr_boot_demo/controller/Controller.java
@@ -1,16 +1,15 @@
 package com.cui.spr_boot_demo.controller;
 
-import com.cui.spr_boot_demo.dto.User;
+import com.cui.spr_boot_demo.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
-@RequestMapping("demo")//类注解，指该 Java 类所在的 rest 请求都必须以这个里面设定的值开头，该类中所有的请求路径都必须以 demo 开头
-@RestController//作为注解，告诉 SpringBoot 这个类是一个 rest 请求的类
+//@RequestMapping("demo")//类注解，指该 Java 类所在的 rest 请求都必须以这个里面设定的值开头，该类中所有的请求路径都必须以 demo 开头
+//@RestController//作为注解，告诉 SpringBoot 这个类是一个 rest 请求的类
 public class Controller {
 
     /**
@@ -85,27 +84,27 @@ public class Controller {
         //使用该方法接收 yaml 里面的变量，需在 main 函数上增加注解 @EnableConfigurationProperties
         System.out.println(user.getName());
         System.out.println(user.getAge());
-        System.out.println(user.getBirthday());
+        //System.out.println(user.getBirthday());
         //list2
         System.out.println("遍历 animals2: ");
         for (Object item : animals2) {
             System.out.println(item);
         }
         //list1
-        System.out.println("遍历 user 对象中的 animals1: ");
-        for (Object animal : this.user.getAnimals1()) {
-            System.out.println(animal);
-        }
+//        System.out.println("遍历 user 对象中的 animals1: ");
+//        for (Object animal : this.user.getAnimals1()) {
+//            System.out.println(animal);
+//        }
         //map1
-        System.out.println("遍历 user 对象中的 fruit1: ");
-        for (Object item : this.user.getFruit1().keySet()) {
-            System.out.println(item + "->" + this.user.getFruit1().get(item));
-        }
+//        System.out.println("遍历 user 对象中的 fruit1: ");
+//        for (Object item : this.user.getFruit1().keySet()) {
+//            System.out.println(item + "->" + this.user.getFruit1().get(item));
+//        }
         //map2
-        System.out.println("遍历 user 对象中的 fruit2: ");
-        for (Object item : this.user.getFruit2().keySet()) {
-            System.out.println(item + "->" + this.user.getFruit2().get(item));
-        }
+//        System.out.println("遍历 user 对象中的 fruit2: ");
+//        for (Object item : this.user.getFruit2().keySet()) {
+//            System.out.println(item + "->" + this.user.getFruit2().get(item));
+//        }
     }
 
 
