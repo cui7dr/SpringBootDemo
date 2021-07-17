ba5628c8aacb337fd3f40e82658c340a88d94062
cui7dr <cui7dr@gmail.com>
2021-07-17 21:10:25 +0800

    自定义 JPA 方法

--- src/main/java/com/cui/spr_boot_demo/dao/UserJpa.java
+++ src/main/java/com/cui/spr_boot_demo/dao/UserJpa.java
@@ -9,6 +9,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository// 表示这个是一个操作数据库的 Repository 类
 
 public interface UserJpa extends JpaRepository<User, Long> {
@@ -17,4 +19,7 @@ public interface UserJpa extends JpaRepository<User, Long> {
     @Transactional// 声明事务管理
     @Query(value = "delete from user where name = :name", nativeQuery = true)// JPA 会将 :name 用参数 @Param 中的名字替换
     public int deleteByName(@Param("name") String name);// 参数变量
+
+    @Query(value = "select * from user where age > ?1",nativeQuery = true)
+    public List<User> findByAge(Integer age);
 }
