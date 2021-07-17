af557bad8e33041b5e9777456c85919c2f73ec14
cui7dr <cui7dr@gmail.com>
2021-07-17 21:09:59 +0800

    自定义方法操作数据库

--- src/main/java/com/cui/spr_boot_demo/controller/UserController.java
+++ src/main/java/com/cui/spr_boot_demo/controller/UserController.java
@@ -21,38 +21,41 @@ public class UserController {
     private UserJpa userJpa;
 
     @GetMapping("findAll")// 查找所有数据
-    public List<User> FindAll() {
+    public List<User> findAll() {
         return this.userJpa.findAll();
     }
 
     @GetMapping("get")// 按主键查找数据
-    public User Get(@RequestParam("id") Long id) {
+    public User get(@RequestParam("id") Long id) {
         return this.userJpa.findById(id).get();
     }
 
     @GetMapping("save")//保存数据
-    public void Save(@RequestBody User user) {// 使用 @RequestBody 接收数据
+    public void save(@RequestBody User user) {// 使用 @RequestBody 接收数据
         userJpa.save(user);
     }
 
     @GetMapping("delete")// 删除数据
-    public void Delete(@RequestParam("id") Long id) {
+    public void delete(@RequestParam("id") Long id) {
         userJpa.deleteById(id);
     }
 
     //新增方法
     @GetMapping("deleteByName")// 根据名称删除
-    public void DeleteByName(@RequestParam("name") String name) {
+    public void deleteByName(@RequestParam("name") String name) {
         this.userJpa.deleteByName(name);
     }
 
     @GetMapping("update")// 更新数据
-    public void Update(@RequestParam("id")Long id,@RequestParam("name")String name){
+    public void update(@RequestParam("id")Long id,@RequestParam("name")String name){
         User user=this.userJpa.findById(id).get();// 先根据 id 将数据 load 出来
         user.setName(name);// 更新数据
         this.userJpa.save(user);
     }
 
     //自定义查询
-
+    @GetMapping("findByAge")// 根据年龄查找数据
+    public List<User> findByAge(@RequestParam("age")Integer age){
+        return this.userJpa.findByAge(age);
+    }
 }
