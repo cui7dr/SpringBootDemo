0f945215605813df7f037d54be6d58d27f181cdb
cui7dr <cui7dr@gmail.com>
2021-07-30 22:54:01 +0800

    添加 RestTemplate

--- src/main/java/com/cui/spr_boot_demo/SprBootDemoApplication.java
+++ src/main/java/com/cui/spr_boot_demo/SprBootDemoApplication.java
@@ -3,8 +3,11 @@ package com.cui.spr_boot_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @EnableConfigurationProperties
@@ -16,8 +19,23 @@ public class SprBootDemoApplication {
     }
 
     @GetMapping("helloworld")
-    public String helloworld(){
+    public String helloworld() {
         return "hello world";
     }
+
+    @Bean
+    public SimpleClientHttpRequestFactory httpClientFactory() {
+        SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
+        httpRequestFactory.setReadTimeout(1000 * 5);
+        httpRequestFactory.setConnectTimeout(1000 * 5);
+        return httpRequestFactory;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(SimpleClientHttpRequestFactory httpClientFactory) {
+        RestTemplate restTemplate = new RestTemplate(httpClientFactory);
+        return new RestTemplate();
+    }
+
 
 }
