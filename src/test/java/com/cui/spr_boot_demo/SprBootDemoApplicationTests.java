bd4ca098abd42d52ac05f47d9a0b67fb9151f619
cui7dr <cui7dr@gmail.com>
2021-08-02 22:22:59 +0800

    添加一个测试方法

--- src/test/java/com/cui/spr_boot_demo/SprBootDemoApplicationTests.java
+++ src/test/java/com/cui/spr_boot_demo/SprBootDemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.cui.spr_boot_demo;
 
+import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)//使用 SpringRunner 加载上下文
 class SprBootDemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test //这是一个 test 单元测试文件
+    public void test1(){
+        int num=new Integer(1);
+        Assert.assertEquals(num,1);//可以简单的理解为 if 条件 = if(num == 1) syso("ok")
     }
 
 }
