ed358eca29957f7029698806aa3e7cb1cf505ddd
cui7dr <cui7dr@gmail.com>
2021-09-28 20:10:59 +0800

    异常处理业务

---
+++ src/main/java/com/cui/spr_boot_demo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.cui.spr_boot_demo.handler;
+
+import com.cui.spr_boot_demo.entity.ResultDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author cui7dr
+ * @date 2021/9/28
+ */
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public ResultDto globalException(HttpServletResponse response, Exception ex) {
+        log.info("ExceptionHandler...");
+        log.info("错误代码：" + response.getStatus());
+        ResultDto resultDto = new ResultDto();
+        resultDto.setCode(0);
+        resultDto.setMsg(ex.getMessage());
+        ;
+        resultDto.setData(null);
+        return resultDto;
+    }
+}
